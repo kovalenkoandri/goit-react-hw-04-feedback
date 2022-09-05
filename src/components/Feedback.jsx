df860ce0fd3c2490fc3c72deac619680864f312e
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-09-05 14:20:20 +0300

    onClick done

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,36 +1,77 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { Text, List, ItemBtn, ItemStat, Rating } from './Feedback.styled';
-const Feedback = () => {
-  return (
-    <>
-      <Text>Please leave feedback</Text>
-      <List>
-        <ItemBtn>
-          <button>Good</button>
-        </ItemBtn>
-        <ItemBtn>
-          <button>Neutral</button>
-        </ItemBtn>
-        <ItemBtn>
-          <button>Bad</button>
-        </ItemBtn>
-      </List>
-      <Text>Statistics</Text>
-      <List>
-        <ItemStat>
-          <Rating>Good: </Rating>
-          <Rating></Rating>
-        </ItemStat>
-        <ItemStat>
-          <Rating>Neutral: </Rating>
-          <Rating></Rating>
-        </ItemStat>
-        <ItemStat>
-          <Rating>Bad: </Rating>
-          <Rating></Rating>
-        </ItemStat>
-      </List>
-    </>
-  );
-};
+class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      valueGood: 0,
+      valueNeutral: 0,
+      valueBad: 0,
+    };
+  }
+  incrementGood = () => {
+    this.setState(state => {
+      return { valueGood: state.valueGood + 1 };
+    });
+  };
+  incrementNeutral = () => {
+    this.setState(state => {
+      return { valueNeutral: state.valueNeutral + 1 };
+    });
+  };
+  incrementBad = () => {
+    this.setState(state => {
+      return { valueBad: state.valueBad + 1 };
+    });
+  };
+  render() {
+    return (
+      <>
+        <Text>Please leave feedback</Text>
+        <List>
+          <ItemBtn>
+            <button
+              onClick={() => {
+                this.incrementGood();
+              }}
+            >
+              Good
+            </button>
+          </ItemBtn>
+          <ItemBtn>
+            <button
+              onClick={() => {
+                this.incrementNeutral();
+              }}
+            >
+              Neutral
+            </button>
+          </ItemBtn>
+          <ItemBtn>
+            <button
+              onClick={() => {
+                this.incrementBad();
+              }}
+            >
+              Bad
+            </button>
+          </ItemBtn>
+        </List>
+        <Text>Statistics</Text>
+        <List>
+          <ItemStat>
+            <Rating>Good: {this.state.valueGood}</Rating>
+          </ItemStat>
+          <ItemStat>
+            <Rating>Neutral: {this.state.valueNeutral}</Rating>
+          </ItemStat>
+          <ItemStat>
+            <Rating>Bad: {this.state.valueBad}</Rating>
+          </ItemStat>
+        </List>
+      </>
+    );
+  }
+}
 export default Feedback;
