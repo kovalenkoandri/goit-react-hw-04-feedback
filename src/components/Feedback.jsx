ebf1694a07a93915c3c8865ab10500caa4172c08
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-09-26 18:20:59 +0300

    styled

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { Section } from './Feedback.styled';
 import { Section } from './Section';
 import { Statistics } from './Statistics';
 import { FeedbackOptions } from './FeedbackOptions';
--- src/components/Feedback.styled.jsx
+++ src/components/Feedback.styled.jsx
@@ -1,19 +1,17 @@
 import styled from 'styled-components';
-export const Section = styled.div`
-  font-size: 30px;
-`;
+
 export const List = styled.ul`
   list-style: none;
   font-size: 16px;
   display: block;
 `;
-  export const ItemBtn = styled.li`
-    display: inline-block;
-    margin-right: 10px;
-    &:nth-of-type(3) {
-      margin-right: 0;
-    }
-  `;
+export const ItemBtn = styled.li`
+  display: inline-block;
+  margin-right: 10px;
+  &:nth-of-type(3) {
+    margin-right: 0;
+  }
+`;
 export const ItemStat = styled.li`
   margin-bottom: 10px;
   &:nth-of-type(5) {
@@ -22,5 +20,6 @@ export const ItemStat = styled.li`
 `;
 export const Rating = styled.span``;
 export const Text = styled.p`
+  background-color: red;
   font-size: 16px;
 `;
