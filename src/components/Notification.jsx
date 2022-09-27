bcbc72edafcb9eaa245c9fe77fbfb2f46dbf8ea1
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-09-27 15:48:28 +0300

    propTypes added

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,4 +1,8 @@
 import { Text } from './Feedback.styled';
+import PropTypes from 'prop-types';
 export const Notification = ({ message }) => {
   return <Text>{message}</Text>;
+};
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
 };
